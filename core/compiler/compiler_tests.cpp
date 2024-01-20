df30147d8d43c9446328f90f52635a2718e7b213
tanner0101 <me@tanner.xyz>
2024-01-20 01:07:46 -0600

    fix test

--- core/compiler/compiler_tests.cpp
+++ core/compiler/compiler_tests.cpp
@@ -5,27 +5,27 @@ xlang::Compiler compiler{};
 
 TEST(CompilerTest, TestCompile) {
     auto diagnostics = Diagnostics();
+    auto tokens = std::vector<Token>{Token{TokenType::identifier, Source{}}};
     auto ast = std::vector<Node>{
         {FunctionDefinition{
              "main", std::vector<Node>{},
              std::vector<Node>{
-                 {FunctionCall{"print",
-                               std::vector<Node>{
-                                   {"Hello, world!", std::vector<Token>{}}}},
-                  std::vector<Token>{}}}},
-         std::vector<Token>{}}};
+                 {FunctionCall{"printf",
+                               std::vector<Node>{{"Hello, world!", tokens}}},
+                  tokens}}},
+         tokens}};
     const auto lli = compiler.compile(ast, diagnostics);
     ASSERT_EQ(lli, R"(; ModuleID = 'xlang'
 source_filename = "xlang"
 
 @0 = private unnamed_addr constant [14 x i8] c"Hello, world!\00", align 1
 
-define i32 @main() {
+declare i32 @printf(i8*, ...)
+
+define i32 @main(i32 %0) {
 entry:
-  %0 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([14 x i8], [14 x i8]* @0, i32 0, i32 0))
+  %1 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([14 x i8], [14 x i8]* @0, i32 0, i32 0))
   ret i32 0
 }
-
-declare i32 @printf(i8*, ...)
 )");
 }
